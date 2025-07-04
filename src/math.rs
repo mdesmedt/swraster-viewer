08917eb9ceaa7ad8f460200d1b08d6284b07dbdb
Matthijs De Smedt <matthijs@anji.nl>
2025-07-04 19:07:24 +0800

    Higher precision rsqrt on NEON

--- src/math.rs
+++ src/math.rs
@@ -8,30 +8,31 @@ use std::arch::x86_64::*;
 
 #[cfg(target_arch = "aarch64")]
 pub fn sqrt_vec(v: Vec4) -> Vec4 {
-    let l_m128: float32x4_t = v.into();
-    let l_m128 = unsafe { vsqrtq_f32(l_m128) };
-    Vec4::from(l_m128)
+    let x: float32x4_t = v.into();
+    let x = unsafe { vsqrtq_f32(x) };
+    Vec4::from(x)
 }
 
 #[cfg(target_arch = "aarch64")]
 pub fn rsqrt_vec(v: Vec4) -> Vec4 {
-    let l_m128: float32x4_t = v.into();
-    let l_m128 = unsafe { vrsqrteq_f32(l_m128) };
-    Vec4::from(l_m128)
+    let x: float32x4_t = v.into();
+    let y0 = unsafe { vrsqrteq_f32(x) };
+    let y1 = unsafe { vmulq_f32(y0, vrsqrtsq_f32(vmulq_f32(x, y0), y0)) };
+    Vec4::from(y1)
 }
 
 #[cfg(target_arch = "x86_64")]
 pub fn sqrt_vec(v: Vec4) -> Vec4 {
-    let l_m128: __m128 = v.into();
-    let l_m128 = unsafe { _mm_sqrt_ps(l_m128) };
-    Vec4::from(l_m128)
+    let x: __m128 = v.into();
+    let x = unsafe { _mm_sqrt_ps(x) };
+    Vec4::from(x)
 }
 
 #[cfg(target_arch = "x86_64")]
 pub fn rsqrt_vec(v: Vec4) -> Vec4 {
-    let l_m128: __m128 = v.into();
-    let l_m128 = unsafe { _mm_rsqrt_ps(l_m128) };
-    Vec4::from(l_m128)
+    let x: __m128 = v.into();
+    let x = unsafe { _mm_rsqrt_ps(x) };
+    Vec4::from(x)
 }
 
 #[cfg(not(any(target_arch = "aarch64", target_arch = "x86_64")))]
